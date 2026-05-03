9674af10c5c30aeb96662303f62f8500830c67ed
Tharasri <tharasri06@gmail.com>
2026-05-03 14:29:39 +0530

    fixed static files

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os 
+
 from pathlib import Path
+
+STATIC_URL = '/static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
